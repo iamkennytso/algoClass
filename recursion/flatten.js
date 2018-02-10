f1efa38b3f8a5e9f96be04b7b08ce8d42e56e628
Kenneth Tso <iamkennytso@gmail.com>
2018-02-10 16:04:27 -0500

    (non-working)

--- recursion/flatten.js
+++ recursion/flatten.js
@@ -4,3 +4,20 @@ Implement a function that flattens a nested array.
 flatten([1,[2],[3, [[4]]]]);
 => [1,2,3,4]
 */
+function flatten (arr) {
+  array = []
+  function recurse(ar){
+    if (typeof ar === 'object'){
+        for (let i = 0; i < ar.length; i++){
+          if (typeof ar[i] === 'object') return recurse(ar[i])
+          else return ar[i]
+        }
+    } else return ar
+  }
+  for(let j = 0; j < arr.length; j++){
+    array.push(recurse(arr[j]))
+  }
+  return array
+}
+
+flatten([1,[2],[3, [[4]]]])
